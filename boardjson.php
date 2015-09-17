02159544c5f2ce9b0c6112484961a3b49211f5a7
parley <parley@314chan.org>
2015-09-17 21:18:06 +0000

    REAL work on boardjson.php. I'll integrate it into the rebuild mod page when done.

--- boardjson.php
+++ boardjson.php
@@ -1,47 +1,36 @@
 <?php
-
+	//include required files
 	include("config.php");
-
     include("inc/mitsuba.php");
-
+    
+    //connect to MySQLi
     $conn = new mysqli($db_host, $db_username, $db_password, $db_database);
-
     $haruko = new Mitsuba($conn);
-
     
-
+    //get all boards
     $sql = "SELECT * FROM `boards`";
-
     
-
     if(!$result = $conn->query($sql)){
-
         die('There was an error running the query [' . $conn->error . ']');
-
     }else{
-
         while($row = $result->fetch_assoc()){
+	        //changing various rows because Vichan's format is a bit more widly used. also reorganizing them, because fuck PHP.
+	        $row["uri"] = $row["short"];
+	        unset($row["short"]);
+	        $row["title"] = $row["name"];
+	        unset($row["name"]);
+	        $row["subtitle"] = $row["message"];
+	        unset($row["message"]);
+	        $row["board_type"] = $row["type"];
+	        unset($row["type"]);
+	        $row["default_name"] = $row["anonymous"];
+	        unset($row["anonymous"]);
+	        $row[""] = $row[""]
+	        
 
-/*            echo("<pre>");
-
-            if($row['strip']){
-
-            echo "Secure Tripcode: ".$row['strip'];
-
-            }
-
-            echo("</pre>");
-
-    }*/
-
-    echo("<pre>");
-
-    //var_dump($row);
-
-    echo json_encode($row);
-
-    echo("</pre>");
-
-    }
-
-    }
+	        $boardArr[] = $row;
+	    }
+	}
+	$finalJSON = json_encode($boardArr);
+	
+	file_put_contents("boards.json",$finalJSON);
