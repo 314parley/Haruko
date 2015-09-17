01891b610a9f44c73ef46f32ca84e0f7a71fd76e
parley <parley@314chan.org>
2015-09-17 15:49:19 +0000

    updated AdminLTE and font awesome.

--- inc/mod/main.inc.php
+++ inc/mod/main.inc.php
@@ -9,7 +9,9 @@
 	$reports = $conn->query("SELECT * FROM reports;")->num_rows;
 
 	$appeals = $conn->query("SELECT * FROM appeals;")->num_rows;
-
+	
+	$bans = $conn->query("SELECT * FROM bans;")->num_rows;
+	
 	$breqs = $conn->query("SELECT * FROM ban_requests;")->num_rows;
 
 	$ppd = $conn->query("SELECT COUNT(*) FROM posts WHERE from_unixtime(date) >= DATE_SUB(NOW(), INTERVAL 1 DAY)");
@@ -448,13 +450,13 @@ if ((isset($_SESSION['logged'])) && ($_SESSION['logged']==1))
 
               <div class="info-box">
 
-                <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+                <span class="info-box-icon bg-orange"><i class="fa fa-flag"></i></span>
 
                 <div class="info-box-content">
 
-                  <span class="info-box-text">Likes</span>
+                  <span class="info-box-text">Reports</span>
 
-                  <span class="info-box-number">LOL</span>
+                  <span class="info-box-number"><?php echo $reports ?></span>
 
                 </div><!-- /.info-box-content -->
 
@@ -474,13 +476,13 @@ if ((isset($_SESSION['logged'])) && ($_SESSION['logged']==1))
 
               <div class="info-box">
 
-                <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+                <span class="info-box-icon bg-red"><i class="fa fa-ban"></i></span>
 
                 <div class="info-box-content">
 
-                  <span class="info-box-text">Sales</span>
+                  <span class="info-box-text">Active Bans</span>
 
-                  <span class="info-box-number">760</span>
+                  <span class="info-box-number"><?php echo $bans ?></span>
 
                 </div><!-- /.info-box-content -->
 
@@ -498,7 +500,7 @@ if ((isset($_SESSION['logged'])) && ($_SESSION['logged']==1))
 
                   <span class="info-box-text">New Members</span>
 
-                  <span class="info-box-number">2,000</span>
+                  <span class="info-box-number">loldunno</span>
 
                 </div><!-- /.info-box-content -->
 
@@ -546,311 +548,7 @@ if ((isset($_SESSION['logged'])) && ($_SESSION['logged']==1))
 
  	  <!-- Tab panes -->
 
- 	  <div class="tab-content">
-
- 		<!-- Home tab content -->
-
- 		<div class="tab-pane" id="control-sidebar-home-tab">
-
- 		  <h3 class="control-sidebar-heading">Recent Activity</h3>
-
- 		  <ul class='control-sidebar-menu'>
-
- 			<li>
-
- 			  <a href='javascript::;'>
-
- 				<i class="menu-icon fa fa-birthday-cake bg-red"></i>
-
- 				<div class="menu-info">
-
- 				  <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-
- 				  <p>Will be 23 on April 24th</p>
-
- 				</div>
-
- 			  </a>
-
- 			</li>
-
- 			<li>
-
- 			  <a href='javascript::;'>
-
- 				<i class="menu-icon fa fa-user bg-yellow"></i>
-
- 				<div class="menu-info">
-
- 				  <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
-
- 				  <p>New phone +1(800)555-1234</p>
-
- 				</div>
-
- 			  </a>
-
- 			</li>
-
- 			<li>
-
- 			  <a href='javascript::;'>
-
- 				<i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
-
- 				<div class="menu-info">
-
- 				  <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
-
- 				  <p>nora@example.com</p>
-
- 				</div>
-
- 			  </a>
-
- 			</li>
-
- 			<li>
-
- 			  <a href='javascript::;'>
-
- 				<i class="menu-icon fa fa-file-code-o bg-green"></i>
-
- 				<div class="menu-info">
-
- 				  <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
-
- 				  <p>Execution time 5 seconds</p>
-
- 				</div>
-
- 			  </a>
-
- 			</li>
-
- 		  </ul><!-- /.control-sidebar-menu -->
-
-
-
- 		  <h3 class="control-sidebar-heading">Tasks Progress</h3>
-
- 		  <ul class='control-sidebar-menu'>
-
- 			<li>
-
- 			  <a href='javascript::;'>
-
- 				<h4 class="control-sidebar-subheading">
-
- 				  Custom Template Design
-
- 				  <span class="label label-danger pull-right">70%</span>
-
- 				</h4>
-
- 				<div class="progress progress-xxs">
-
- 				  <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-
- 				</div>
-
- 			  </a>
-
- 			</li>
-
- 			<li>
-
- 			  <a href='javascript::;'>
-
- 				<h4 class="control-sidebar-subheading">
-
- 				  Update Resume
-
- 				  <span class="label label-success pull-right">95%</span>
-
- 				</h4>
-
- 				<div class="progress progress-xxs">
-
- 				  <div class="progress-bar progress-bar-success" style="width: 95%"></div>
-
- 				</div>
-
- 			  </a>
-
- 			</li>
-
- 			<li>
-
- 			  <a href='javascript::;'>
-
- 				<h4 class="control-sidebar-subheading">
-
- 				  Laravel Integration
-
- 				  <span class="label label-waring pull-right">50%</span>
-
- 				</h4>
-
- 				<div class="progress progress-xxs">
-
- 				  <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
-
- 				</div>
-
- 			  </a>
-
- 			</li>
-
- 			<li>
-
- 			  <a href='javascript::;'>
-
- 				<h4 class="control-sidebar-subheading">
-
- 				  Back End Framework
-
- 				  <span class="label label-primary pull-right">68%</span>
-
- 				</h4>
-
- 				<div class="progress progress-xxs">
-
- 				  <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
-
- 				</div>
-
- 			  </a>
-
- 			</li>
-
- 		  </ul><!-- /.control-sidebar-menu -->
-
-
-
- 		</div><!-- /.tab-pane -->
-
- 		<!-- Stats tab content -->
-
- 		<div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div><!-- /.tab-pane -->
-
- 		<!-- Settings tab content -->
-
- 		<div class="tab-pane" id="control-sidebar-settings-tab">
-
- 		  <form method="post">
-
- 			<h3 class="control-sidebar-heading">General Settings</h3>
-
- 			<div class="form-group">
-
- 			  <label class="control-sidebar-subheading">
-
- 				Report panel usage
-
- 				<input type="checkbox" class="pull-right" checked />
-
- 			  </label>
-
- 			  <p>
-
- 				Some information about this general settings option
-
- 			  </p>
-
- 			</div><!-- /.form-group -->
-
-
-
- 			<div class="form-group">
-
- 			  <label class="control-sidebar-subheading">
-
- 				Allow mail redirect
-
- 				<input type="checkbox" class="pull-right" checked />
-
- 			  </label>
-
- 			  <p>
-
- 				Other sets of options are available
-
- 			  </p>
-
- 			</div><!-- /.form-group -->
-
-
-
- 			<div class="form-group">
-
- 			  <label class="control-sidebar-subheading">
-
- 				Expose author name in posts
-
- 				<input type="checkbox" class="pull-right" checked />
-
- 			  </label>
-
- 			  <p>
-
- 				Allow the user to show his name in blog posts
-
- 			  </p>
-
- 			</div><!-- /.form-group -->
-
-
-
- 			<h3 class="control-sidebar-heading">Chat Settings</h3>
-
-
-
- 			<div class="form-group">
-
- 			  <label class="control-sidebar-subheading">
-
- 				Show me as online
-
- 				<input type="checkbox" class="pull-right" checked />
-
- 			  </label>
-
- 			</div><!-- /.form-group -->
-
-
-
- 			<div class="form-group">
-
- 			  <label class="control-sidebar-subheading">
-
- 				Turn off notifications
-
- 				<input type="checkbox" class="pull-right" />
-
- 			  </label>
-
- 			</div><!-- /.form-group -->
-
-
-
- 			<div class="form-group">
-
- 			  <label class="control-sidebar-subheading">
-
- 				Delete chat history
-
- 				<a href="javascript::;" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
-
- 			  </label>
-
- 			</div><!-- /.form-group -->
-
- 		  </form>
-
- 		</div><!-- /.tab-pane -->
-
- 	  </div>
+ 	  
 
  	</aside><!-- /.control-sidebar -->
 
@@ -870,7 +568,7 @@ if ((isset($_SESSION['logged'])) && ($_SESSION['logged']==1))
 
    <!-- Bootstrap 3.3.2 JS -->
 
-   <script src="/js/bootstrap.min.js" type="text/javascript"></script>
+   <script src="/js/bootstrap.js" type="text/javascript"></script>
 
    <!-- SlimScroll -->
 
